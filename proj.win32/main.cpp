00f36ee841085706c6cc5f30723ea325d89d28e3
jordan13game <403562789@qq.com>
2014-04-03 16:38:39 +0800

    test

--- proj.win32/main.cpp
+++ proj.win32/main.cpp
@@ -16,6 +16,6 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     AppDelegate app;
     CCEGLView* eglView = CCEGLView::sharedOpenGLView();
     eglView->setViewName("Game");
-    eglView->setFrameSize(480, 320);
+    eglView->setFrameSize(360,640);
     return CCApplication::sharedApplication()->run();
 }
